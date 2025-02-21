9b1fcb54825a377c6c06bb4b82fa5eb05597227c
ali-s3 <ghulamali43222@gmail.com>
2025-02-21 21:59:27 +0500

    adding new folders and files to file tree

---
+++ app/components/FileEditor/CustomTreeDataProvider.ts
@@ -0,0 +1,45 @@
+import { TreeDataProvider, TreeItemIndex, TreeItem, Disposable } from 'react-complex-tree';
+import { longTree } from '@/app/utils/storage/fileTreeData';
+
+export default class CustomTreeDataProvider implements TreeDataProvider {
+    private data: Record<TreeItemIndex, TreeItem> = { ...longTree.items };
+
+    private treeChangeListeners: ((changedItemIds: TreeItemIndex[]) => void)[] =
+        [];
+
+    public async getTreeItem(itemId: TreeItemIndex) {
+        return this.data[itemId];
+    }
+
+    public async onChangeItemChildren(
+        itemId: TreeItemIndex,
+        newChildren: TreeItemIndex[]
+    ) {
+        this.data[itemId].children = newChildren;
+        this.treeChangeListeners.forEach(listener => listener([itemId]));
+    }
+
+    public onDidChangeTreeData(listener: (changedItemIds: TreeItemIndex[]) => void): Disposable {
+        this.treeChangeListeners.push(listener);
+        return {
+            dispose: () =>
+                this.treeChangeListeners.splice(
+                    this.treeChangeListeners.indexOf(listener),
+                    1
+                ),
+        };
+    }
+
+    public async onRenameItem(item: TreeItem<any>, name: string): Promise<void> {
+        this.data[item.index].data = name;
+    }
+
+    // custom handler for directly manipulating the tree data
+    public injectItem(name: string) {
+        console.log('data: ', this.data);
+        const rand = `${Math.random()}`;
+        this.data[rand] = { data: name, index: rand } as TreeItem;
+        this.data.root.children?.push(rand); // this means that if the root has children, push the new item to the children array
+        this.treeChangeListeners.forEach(listener => listener(['root']));
+    }
+}
--- app/components/FileEditor/FileTree.tsx
+++ app/components/FileEditor/FileTree.tsx
@@ -1,55 +1,10 @@
 "use client";
-import { useMemo } from 'react';
-import { UncontrolledTreeEnvironment, Tree, TreeDataProvider, TreeItemIndex, TreeItem } from 'react-complex-tree';
+import { UncontrolledTreeEnvironment, Tree } from 'react-complex-tree';
 import 'react-complex-tree/lib/style-modern.css';
-import { longTree } from '@/app/utils/storage/FileTree';
-
-class CustomDataProviderImplementation implements TreeDataProvider {
-  private data: Record<TreeItemIndex, TreeItem> = { longTree };
-
-  private treeChangeListeners: ((changedItemIds: TreeItemIndex[]) => void)[] =
-    [];
-
-  public async getTreeItem(itemId: TreeItemIndex) {
-    return this.data[itemId];
-  }
-
-  public async onChangeItemChildren(
-    itemId: TreeItemIndex,
-    newChildren: TreeItemIndex[]
-  ) {
-    this.data[itemId].children = newChildren;
-    this.treeChangeListeners.forEach(listener => listener([itemId]));
-  }
-
-  // public onDidChangeTreeData(
-  //   listener: (changedItemIds: TreeItemIndex[]) => void
-  // ): Disposable {
-  //   this.treeChangeListeners.push(listener);
-  //   return {
-  //     dispose: () =>
-  //       this.treeChangeListeners.splice(
-  //         this.treeChangeListeners.indexOf(listener),
-  //         1
-  //       ),
-  //   };
-  // }
-
-  public async onRenameItem(item: TreeItem<any>, name: string): Promise<void> {
-    this.data[item.index].data = name;
-  }
-
-  // custom handler for directly manipulating the tree data
-  public injectItem(name: string) {
-    const rand = `${Math.random()}`;
-    this.data[rand] = { data: name, index: rand } as TreeItem;
-    this.data.root.children?.push(rand);
-    this.treeChangeListeners.forEach(listener => listener(['root']));
-  }
-}
+import CustomTreeDataProvider from './CustomTreeDataProvider';
 
 export default function FileTree() {
-  const dataProvider = new CustomDataProviderImplementation();
+  const dataProvider = new CustomTreeDataProvider();
   return (
     <UncontrolledTreeEnvironment
       canDragAndDrop
